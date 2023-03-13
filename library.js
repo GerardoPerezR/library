a9e556d5fda475cec427332a0ae6562f02970167
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-03-13 12:31:01 -0600

    UPDATE JS AND CSS

--- library.js
+++ library.js
@@ -1,1 +1,19 @@
-// insert js here
+
+let myLibrary = [];
+
+//book object constructor
+function Book(title, author, pages, isbn, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isbn = isbn;
+  this.read = read;
+
+}
+
+// add book to library
+
+
+function addBook()  {
+
+}
