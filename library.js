518a3b4d7719821f6f8b4ed8d56dbb7de856addc
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-03-14 18:54:40 -0600

    LOOKING GOOD

--- library.js
+++ library.js
@@ -1,5 +1,6 @@
 
-let myLibrary = [];
+
+var bookId = 0;
 
 //book object constructor
 function Book(title, author, pages, isbn, read) {
@@ -8,12 +9,44 @@ function Book(title, author, pages, isbn, read) {
   this.pages = pages;
   this.isbn = isbn;
   this.read = read;
+  
 
 }
 
-// add book to library
+
+const book1 = new Book('Don Quijote', 'Cervantes', 587, 34623, true);
+const book2 = new Book('Ulysses', 'James Joyce', 236, 247623, false);
+
+const myLibrary = [book1, book2];
 
 
-function addBook()  {
+const form = document.querySelector('#newBookForm')
+Book.prototype.sayInfo = function() {
+  console.log(`${title}written by${author}is${pages}long`)};
 
+//create book card
+
+
+
+
+const createBookCard = (book) => {
+  return ` <div class="card" style="height: auto; width: 18rem;">
+<h5 class="card-title">Book1</h5>
+<p class="card-text">
+  <ul class="list-group">
+    <li class="list-group-item">Title: ${book.title}</li>
+    <li class="list-group-item">Author: ${book.author}</li>
+    <li class="list-group-item">ISBN: ${book.pages}</li>
+    <li class="list-group-item">Pages: ${book.isbn}</li>
+    <li class="list-group-item">Read: ${book.read}</li>
+  </ul>
+</p>
+</div>`;
+}
+
+  // add book to library
+
+
+function addBook(title, author, pages, isbn, read, id)  {
+          const book = new Book(title, author, pages, isbn, read);
 }
