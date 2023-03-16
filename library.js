da16361e10d627942fca518218c9d766190cb03e
Gerardo Perez Rocha <gerardoperezrocha@gmail.com>
2023-03-15 18:02:59 -0600

    It's Alive eventlisteners begin to work

--- library.js
+++ library.js
@@ -1,6 +1,5 @@
 
 
-var id = 1;
 
 
 
@@ -17,15 +16,15 @@ function Book(title, author, pages, isbn, read,id) {
 }
 
 
-const book1 = new Book('Don Quijote', 'Cervantes', 587, 34623, false, 1);
+const book1 = new Book('Don Quijote', 'Cervantes', 587, 34623, true, 1, );
 const book2 = new Book('Ulysses', 'James Joyce', 236, 247623, true, 2);
 
-const myLibrary = [book1, book2,];
+const myLibrary = [book1, book2];
 
 
 
-Book.prototype.sayInfo = function() {
-  console.log(`${title}written by${author}is${pages}long`)};
+
+
 
 
 //create book card
@@ -35,35 +34,48 @@ Book.prototype.sayInfo = function() {
 
 
 const createBookCard = (book) => {
+
   if (book.read == true)  {
-    var readStatus = "checked"
+    var readStatusSwitch = "checked"
   }
   else{
-    var readStatus = "default"
+    var readStatusSwitch = "default"
   }
-  return ` <div class="card" style="height: auto; width: 18rem;">
-<h5 class="card-title">Book${id}</h5>
-<p class="card-text">
-  <ul class="list-group">
-    <li class="list-group-item">Title: ${book.title}</li>
-    <li class="list-group-item">Author: ${book.author}</li>
-    <li class="list-group-item">ISBN: ${book.pages}</li>
-    <li class="list-group-item">Pages: ${book.isbn}</li>
-    <li class="list-group-item">Read: ${book.read}</li>
-    <div class="form-check form-switch">
-  <input class="form-check-input" type="checkbox" id="readCheckbox${id}" data-book-id="${book.id}", data-read-status="${book.read}",  ${readStatus}>
-  <label class="form-check-label" for="readCheckbox">Read</label>
-</div>
-</div>
-  </ul>
-</p>
-</div>`;
+
+  const bookCard = ` <div class="card" style="height: auto; width: 18rem;">
+  <h5 class="card-title">Book${book.id}</h5>
+  <p class="card-text">
+    <ul class="list-group">
+      <li class="list-group-item">Title: ${book.title}</li>
+      <li class="list-group-item">Author: ${book.author}</li>
+      <li class="list-group-item">ISBN: ${book.pages}</li>
+      <li class="list-group-item">Pages: ${book.isbn}</li>
+      <li class="list-group-item">Read: ${book.read}</li>
+      <div class="form-check form-switch">
+      <button type="button" class="btn btn-secondary" data-book-id="${book.id}" data-read-status="${book.read}">Toggle Read Status</button>
+      
+  </div>
+    </ul>
+  </p>
+  </div>`
+;
+    
+
+
+
+
+
+  return bookCard;
 }
 
 
 //Create eventlistener for read switch to change read 
 
-const readSwitch = document.getElementById(`readCheckbox${id}`)
+const eventListenerArray = [];
+
+
+
+//const readSwitch = document.getElementById(`readCheckbox${id}`)
 
 
 
